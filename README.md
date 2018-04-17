81333d7df57b76b93e6f18c64cf283b814ffd9c4
calebking3 <calebking3@gmail.com>
2018-04-16 19:29:47 -0700

    adding about information

---
+++ README.md
@@ -0,0 +1,4 @@
+# People In Tech Landing Page
+
+
+This example is the react-static landing page for the people in tech podcast.
---
+++ src/containers/About.js
@@ -0,0 +1,28 @@
+import React from 'react'
+//
+import Typography from 'material-ui/Typography'
+
+export default () => (
+  <div>
+    <Typography type="headline" gutterBottom>
+      The People In Tech podcast was started by Caleb King in 2018 with the idea of breaking town technology topics in a manner all can interpret and understand.
+      Targeted towards people in tech, people interested in getting into tech or who are just interested in the specific topic we address. I started the podcast
+      as a fun project but with the growth of our listeners also known as "Day1 Listeners" we have created a community where all tech related questions can be asked
+      without any judgement. Our motto is, "We are here for you". For the people, by the people delivered to the people. Join the conversation! 
+    </Typography>
+    <Typography type="body1">
+      Caleb King
+
+      Caleb is the founder of Devboxx LLC a technology solutions company. Prior to starting the People In Tech podcast,
+      Caleb works and still is a software engineer, developing web and mobile applications for enterprise and startup companies. 
+      His passion for technology started a young age watching his uncle build computers for his tech support role. 
+      After receiving his Bachelors in Business from Arizona State University, he decided to pursue technical roles to build his skillset. 
+      Starting off as a e-learning developer, working his way into software engineer, Caleb was always the technical person in his peer group. 
+      Once Anchor.fm launched their app, Caleb finally had a platform to begin podcasting and reaching an audience. 
+
+      His passion for helping people led Caleb to become a mentor thru Thinkful.com to help future developers break into the industry. His favorite categories
+      of technology are Javascript development, Aritifcial Intelligence and Cybersecurity. Outside of his day job, Caleb enjoys health, fitness, sports, especially
+      basketball and spending time with family and friends. Enjoy! You nerds!
+    </Typography>
+  </div>
+)
